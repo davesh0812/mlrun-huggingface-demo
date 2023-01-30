dd44cf69f7e19f803e1dec6ef8a4c99b1f4abd6d
davesh0812 <davesh0812@gmail.com>
2023-01-30 15:58:01 +0200

    .

--- src/serving_test.py
+++ src/serving_test.py
@@ -48,7 +48,7 @@ def model_server_tester(
         event_data = dataset.iloc[i].to_dict()["text"]
         try:
             start = datetime.now()
-            resp = requests.post(f"{endpoint}/predict", data=event_data)
+            resp = requests.post(f"{endpoint}/predict", data=event_data.encode('utf-8'))
             if not resp.ok:
                 context.logger.error(f"bad function resp!!\n{resp.text}")
                 err_count += 1
