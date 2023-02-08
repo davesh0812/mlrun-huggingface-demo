b5981e263a7388f26df11d0591d12b88da432775
davesh0812 <davesh0812@gmail.com>
2023-02-08 13:33:55 +0200

    serving

--- src/onnx_model_server.py
+++ src/onnx_model_server.py
@@ -42,6 +42,29 @@ def _get_model_dir(model_uri: str):
     return model_dir, model_artifact.extra_data["tokenizer"]
 
 
+def preprocess(text: Union[str, Dict]) -> Dict:
+    """
+    Converting a simple text into a structured body for the serving function
+
+    :param text: The text to predict
+    """
+    return {"inputs": [str(text)]}
+
+
+def postprocess(model_response: Dict) -> List:
+    """
+    Transfering the prediction to the gradio interface.
+
+    :param model_response: A dict with the model output
+    """
+    output = model_response["outputs"][0]
+    prediction = LABELS[int(output["label"])]
+    return [
+        "The sentiment is " + prediction,
+        "The prediction score is " + str(output["score"]),
+    ]
+
+
 class ONNXModelServer(V2ModelServer):
     """
     ONNX Model serving class, inheriting the V2ModelServer class for being initialized automatically by the model server
