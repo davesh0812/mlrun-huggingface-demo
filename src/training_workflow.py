b01f2bbffe718f1e5f0ec49f67e2913dd25cdef0
davesh0812 <davesh0812@gmail.com>
2023-02-14 16:34:54 +0200

    .

--- src/training_workflow.py
+++ src/training_workflow.py
@@ -44,17 +44,12 @@ def kfpipeline(
 
     # Get the function:
     serving_function = project.get_function("serving")
-    graph = serving_function.spec.graph
-
-    # Build the serving graph:
-    graph.to(handler="preprocess_optimize", name="preprocess").to(
-        class_name="ONNXModelServer",
-        name="sentiment-analysis",
-        model_path=optimization_run.outputs["model"],
-    ).to(handler="postprocess_optimize", name="postprocess").respond()
+    serving_function.spec.graph["sentiment-analysis"].class_args["model_path"] = str(
+        optimization_run.outputs["model"]
+    )
 
     # Deploy the serving function:
-    deploy_return = mlrun.deploy_function(serving_function)
+    deploy_return = mlrun.deploy_function("serving")
 
     # Model server tester
     mlrun.run_function(
