0247b1baacc1db6010299f97e177d5e55b4dafda
davesh0812 <davesh0812@gmail.com>
2023-02-26 11:58:03 +0200

    setup

--- src/project_setup.py
+++ src/project_setup.py
@@ -4,6 +4,8 @@ import mlrun
 def create_and_set_project(
     name: str = "huggingface",
     git_source: str = "git://github.com/davesh0812/mlrun-huggingface-demo.git#main",
+    default_image: str = "mlrun/ml-models",
+    requirements: str = ['transformers', 'datasets'],
     user_project=False,
 ):
     # get/create a project and register the data prep and trainer function in it
@@ -11,26 +13,28 @@ def create_and_set_project(
 
     project.set_source(git_source, pull_at_runtime=True)
 
-    project.set_function(
-        name="data-prep",
-        image="davesh0812/mlrun:mlrun-huggingface-demo-1",
-        handler="src.data_prep.prepare_dataset",
-        kind="job",
-        with_repo=True,
-    )
+    # project.set_function(
+    #     name="data-prep",
+    #     image=default_image,
+    #     handler="src.data_prep.prepare_dataset",
+    #     kind="job",
+    #     with_repo=True,
+    # )
     project.set_function(
         name="trainer",
-        image="davesh0812/mlrun:mlrun-huggingface-demo-1",
+        image=default_image,
         handler="src.trainer.train",
         kind="job",
         with_repo=True,
+        requirements=requirements
     )
     project.set_function(
         name="optimizer",
-        image="davesh0812/mlrun:mlrun-huggingface-demo-1",
+        image=default_image,
         handler="src.trainer.optimize",
         kind="job",
         with_repo=True,
+        requirements=requirements
     )
     project.set_function(
         name="server-tester",
@@ -38,7 +42,21 @@ def create_and_set_project(
         handler="src.serving_test.model_server_tester",
         kind="job",
         with_repo=True,
+        requirements=requirements
     )
+
+    serving_function = mlrun.new_function("serving", kind="serving", image="mlrun/ml-models", requirements=requirements)
+    project.set_function(serving_function)
+
+    serving_function_staging = mlrun.code_to_function(
+        filename="src/serving.py",
+        name="serving",
+        tag="staging",
+        kind="serving",
+        image=default_image,
+        requirements=requirements
+    )
+    project.set_function(serving_function_staging)
     
     project.set_workflow("training_workflow", "src/training_workflow.py")
     project.save()
