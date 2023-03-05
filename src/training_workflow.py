9b0e3635a9df3f3d2f41df7a481e7971266f0896
davesh0812 <davesh0812@gmail.com>
2023-03-05 13:40:56 +0200

    .

--- src/training_workflow.py
+++ src/training_workflow.py
@@ -16,6 +16,7 @@ def kfpipeline(
         function="data-prep",
         params={"dataset_name": dataset_name},
         outputs=["train_dataset", "test_dataset"],
+        auto_build=True
     )
 
     # Training:
